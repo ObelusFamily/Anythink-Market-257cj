ba239fc70a734f0c0080397eb00c7d5e60f09c3b
Bernhard Rapp <bernhard.rapp@student.tugraz.at>
2022-08-14 18:41:16 +0200

    #2, trieing random things to pass tests

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,6 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
+  item.image = item.image === "" ? "./placeholder.png" : item.image;
 
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -36,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image === "" ? "./placeholder.png" : item.image}
+        src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
