4ccdf7f20e9016f7ab2c35939c073fb6a6a5ef9e
Bernhard Rapp <bernhard.rapp@student.tugraz.at>
2022-08-14 18:46:59 +0200

    Revert "#2, trieing random things to pass tests"
    This reverts commit ba239fc70a734f0c0080397eb00c7d5e60f09c3b.

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,7 +19,6 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
-  item.image = item.image === "" ? "./placeholder.png" : item.image;
 
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -37,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image === "" ? "./placeholder.png" : item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
